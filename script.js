382fef8b8ed927119c0ceba466859d1f2591f9d3
unknown <mariana10sandoval@gmail.com>
2025-04-01 19:39:05 -0700

    i think what the document.querySelector() does is that it chooses the elements from index.html so that the JavaScript can connect with each other. i am unsure if i did this correctly but from what i read that is what makes it easy for me to make sense of it.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let titleElement = document.getElementById("title");
-
 console.log(titleElement);
+
+let paragraph = document.querySelector("#paragraph");
+console.log(paragraph);
+
+let header3 = document.querySelector("#header3");
+console.log(header3);
+
+let header4 = document.querySelector("#header4");
+console.log(header4);
