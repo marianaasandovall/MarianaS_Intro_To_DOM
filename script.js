3cb47c8e77d60ad14f0872ca7675d4f27208365d
unknown <mariana10sandoval@gmail.com>
2025-04-04 12:59:30 -0700

    fixed code on script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let titleElement = document.getElementById("title");
 console.log(titleElement);
 
-let paragraph = document.querySelector("#paragraph");
+let paragraph = document.querySelector("p");
 console.log(paragraph);
 
-let header3 = document.querySelector("#header3");
+let header3 = document.querySelector(".header3");
 console.log(header3);
 
-let header4 = document.querySelector("#header4");
+let header4 = document.querySelector(".header4");
 console.log(header4);
+
+
